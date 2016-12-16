59511f7018525326fad9cbd346623564ac0b3091
WilliamNguyen0310 <joenguyen@Joes-MacBook-Pro.local>
2016-12-16 23:33:13 +0200

    Initialize app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const express = require('express');
+const hbs = require('hbs');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+app = express();
+
+app.set('port', process.env.PORT || 5000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+// bodyParser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Server static file
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(app.get('port'), () => {
+  console.log('Listening on port', app.get('port'));
+});
