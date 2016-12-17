9b592f38c882eb8dd21900db29849421ed6005b2
Joe Nguyen <ntuandung93@gmail.com>
2016-12-18 01:20:56 +0200

    Add authentication method

--- app.js
+++ app.js
@@ -34,30 +34,80 @@ app.use(session({
   cookieName: 'session',
   secret: 'dsfasdf1239@%*@#*@zzcb##<>:P',
   duration: 30 * 60 * 1000,
-  activeDuration: 5 * 60 *1000
+  activeDuration: 5 * 60 * 1000
 }));
 
+const requireLogin = (req, res, next) => {
+  if (!req.session.user) {
+    res.redirect('/');
+  } else {
+    next();
+  }
+};
+
 app.get('/', (req, res) => {
-  fs.readFile('./public/json/money_spent.json', 'utf-8', (err, data) => {
-    moneySpentList = JSON.parse(data);
-    moneySpentList.forEach((eachSpending) => {
-      eachSpending.date = new Date(eachSpending.date);
-    });
-    res.render('home.hbs', {moneySpentList,});
-  });
+  if (req.session.user) {
+    res.redirect('/showSpending');
+  } else {
+    res.render('home.hbs');
+  }
 });
 
-app.get('/addSpending', (req, res) => {
+app.get('/addSpending', requireLogin, (req, res) => {
   res.render('add_spending.hbs');
 });
 
-app.post('/addSpending', (req, res) => {
+app.post('/addSpending', requireLogin, (req, res) => {
   const newSpending = {
     amount: Number(req.body.amount),
     description: req.body.description,
   };
   newSpending.date = req.body.date ? new Date(req.body.date) : new Date();
   addNewSpending(newSpending);
+  res.redirect('/');
+});
+
+app.get('/showSpending', requireLogin, (req, res) => {
+  fs.readFile('./public/json/money_spent.json', 'utf8', (err, data) => {
+    moneySpentList = JSON.parse(data);
+    moneySpentList.forEach((eachSpending) => {
+      eachSpending.date = new Date(eachSpending.date);
+    });
+    res.render('show_spending.hbs', {moneySpentList,});
+  });
+});
+
+app.get('/login', (req, res) => {
+  res.render('login.hbs');
+});
+
+app.post('/login', (req, res) => {
+  const user = {
+    username: req.body.username,
+    password: req.body.password,
+  };
+  fs.readFile('user_accs.json', 'utf8', (err, data) => {
+    if (err) {
+      throw err;
+      res.send('Error. Sorry');
+    } else {
+      const userInfo = JSON.parse(data);
+      userInfo.forEach((eachUser) => {
+        if (eachUser.username === user.username) {
+          if (eachUser.password === user.password) {
+            req.session.user = user;
+            res.redirect('/showSpending');
+          }
+        } else {
+          res.redirect('/');
+        }
+      });
+    }
+  });
+});
+
+app.get('/logout', (req, res) => {
+  req.session.reset();
   res.redirect('/');
 });
 
